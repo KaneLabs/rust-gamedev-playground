55c3839e8d870b8a8a50fede76844f779a909949
kanelabs <ryan@kanelabs.com>
2023-06-03 12:12:07 -0500

    hello ECS

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
-
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run();
 }
-
 
 // System
 // Added to App's Schedule: https://docs.rs/bevy_ecs/latest/bevy_ecs/schedule/struct.Schedule.html
 fn hello_world() {
     println!("hello world!");
+}
+
+// Components
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    let rees_bundle = (Person, Name("Rees LaBreee".to_string()));
+    let andy_bundle = (Person, Name("Andy Buckhovich".to_string()));
+    let ryan_bundle = (Person, Name("Ryan Kane".to_string()));
+    commands.spawn(rees_bundle);
+    commands.spawn(andy_bundle);
+    commands.spawn(ryan_bundle);
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
